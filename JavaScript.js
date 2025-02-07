d64cf738d577c02a52dc49b4600439d263f95c76
cas1092 <castillo.goche.l@gmail.com>
2025-02-07 16:49:47 -0500

    Initial commit

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+﻿const textarea = document.getElementById('textarea');
+const charCountDisplay = document.getElementById('charCount');
+const maxChars = 250;
+const msg_area = document.getElementById('message_container');
+
+textarea.addEventListener('input', function () {
+    const currentLength = textarea.value.length;
+
+    charCountDisplay.textContent = `${currentLength} / ${maxChars}`;
+
+    if (currentLength >= maxChars) {
+        textarea.classList.add('limit');
+        charCountDisplay.classList.add('limit');
+        
+
+    } else {
+        textarea.classList.remove('limit');
+        charCountDisplay.classList.remove('limit');
+        
+    }
+});
